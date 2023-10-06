c092803dbf6b936e865523206e2fb7aef489e16d
Joel Tari <joel.tari31@gmail.com>
2023-10-06 10:21:04 +0200

    .

--- src/ManifoldExtras.jl
+++ src/ManifoldExtras.jl
@@ -306,6 +306,9 @@ function Jrinv(sk::se2)
   J33=1
   SA_F64[J11 J12 J13;J21 J22 J23;J31 J32 J33]
 end
+function Jrinv(isomorph_to_se2::SVector{3,Float64})
+  Jrinv(se2(isomorph_to_se2...))
+end
 
 "Jr"
 function Jr(sk::se2)
