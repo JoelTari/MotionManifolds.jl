a35c23dc7a5e02a5c295370c30e8aad3f471b0a8
Joel Tari <joel.tari31@gmail.com>
2025-06-27 18:06:11 +0200

    bump v2.1.6

--- src/MotionManifolds.jl
+++ src/MotionManifolds.jl
@@ -182,7 +182,6 @@ struct SO3
         SO3(R::SMatrix{3,3,Float64,9})
     """
     function SO3(R::SMatrix{3,3,Float64,9})
-        # FIX: use quaternions as intermediary to build R
         Rskew=R-R'
         @assert is_skew(Rskew)
         trace=R[1, 1]+R[2, 2]+R[3, 3]
